b0412d70d0f274c515e61109c102c22e2fce3d85
rafaelzv770 <tankpakekacamata@gmail.com>
2026-04-21 19:40:04 +0700

    memperbaiki error

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,12 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use Illuminate\Suppolort\Facades\Route;
 
 Route::get('/', function () {
     return view('login');
 });
 Route::get('/dashboard', function () {
     return view('dashboard');
-});
-Route::get('/login', function () {
-    return view('login');
 });
 Route::get('/register', function () {
     return view('register');
